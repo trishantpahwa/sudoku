e48d25dbce952c3399bcd69ed87d4f57456e4ea8
Trishant Pahwa <trishantpahwa@gmail.com>
2020-09-22 13:33:38 +0530

    Fixed checking functions.

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,12 @@
 var matrix = [ ];
 
-// for(var i=1;i<=9;i++) {
-//     for(var j=1;j<=9;j++) {
-//         // document.getElementById((i-1)*9+j).value = j; //Columns
-//         // document.getElementById(i+(j-1)*9).value = j; //Rows
-//         // document.getElementById((i-1)*9+j).value = (i-1)*9+j; 
-//     }
-// }
+for(var i=1;i<=9;i++) {
+    for(var j=1;j<=9;j++) {
+        // document.getElementById((i-1)*9+j).value = j; //Columns
+        // document.getElementById(i+(j-1)*9).value = j; //Rows
+        document.getElementById((i-1)*9+j).value = (i-1)*9+j; 
+    }
+}
 
 function getAllValues() {
     for(var i=1;i<=81;i++) {
@@ -15,37 +15,39 @@ function getAllValues() {
     }
 }
 
-function checkColumns() {
-    var errorColumns = [];
-    for(var i=1;i<=9;i++) {
-        var rowData = new Set();
-        for(var j=1;j<=9;j++) {
-            rowData.add(matrix[(i-1)*9+j]);
+function checkRows() {
+    var errorRows = [];
+    for(var i=0;i<9;i++) {
+        var row = [ ];
+        for(var j=0;j<9;j++) {
+            row.push(matrix[i*9+j]);
         }
+        const rowData = new Set(row);
         if(rowData.size < 9) {
-            errorColumns.push(i);
+            errorRows[i] = row;
         }
     }
-    if(errorColumns.length > 0) {
-        return errorColumns;
+    if(Object.keys(errorRows).length > 0) {
+        return errorRows;
     } else {
         return true;
     }
 }
 
-function checkRows() {
-    var errorRows = [];
-    for(var i=1;i<=9;i++) {
-        var rowData = new Set();
-        for(var j=1;j<=9;j++) {
-            rowData.add(matrix[i+(j-1)*9]);
+function checkColumns() {
+    var errorColumns = {};
+    for(var i=0;i<9;i++) {
+        var col = [ ];
+        for(var j=0;j<9;j++) {
+            col.push(matrix[i+j*9]);
         }
-        if(rowData.size < 9) {
-            errorRows.push(i);
+        const columnData = new Set(col);
+        if(columnData.size < 9) {
+            errorColumns[i] = col;
         }
     }
-    if(errorRows.length > 0) {
-        return errorRows;
+    if(Object.keys(errorColumns).length > 0) {
+        return errorColumns;
     } else {
         return true;
     }
@@ -54,38 +56,29 @@ function checkRows() {
 function checkMiniBox() {
     var errorBoxes = { };
     var boxes = [ ];
-    for(var i=0;i<81;i+=27) {
-        for(var j=i;j<i+27;j+=3) {
+    for(var i=0;i<81;i+=27) {  // Rows - 3 at a time
+        for(var j=i;j<i+9;j+=3) {  // Columns - 3 at a time
             var box = [ ];
-            for(var k=j;k<i+27;k+=27) {
-                box.push(matrix[k]);
-                box.push(matrix[k+1]);
-                box.push(matrix[k+2]);
+            var flag = false;
+            for(var k=j;k<j+27;k+=9) {
+                var smallRow = [ ];
+                for(var l=k;l<k+3;l++) {
+                    smallRow.push(matrix[l]);
+                }
+                box.push(...smallRow);
             }
             boxes.push(box);
         }
     }
-    var finalBoxes = [ ];
     for(var i=0;i<9;i++) {
-        box = [ ];
-        for(var j=i;j<i+9;j+=3) {
-            box.push(boxes[j])
-        }
-        finalBoxes.push(box);
-    }
-    boxes = [ ];
-    for(var i=0;i<9;i++) {
-        box = [ ...finalBoxes[i][0], ...finalBoxes[i][1], ...finalBoxes[i][2] ]
-        boxes.push(box);
-    }
-    for(var i=0;i<9;i++) {
-        var boxTest = new Set(boxes[i]);
-        if(boxTest.size < 9) {
-            errorBoxes[i] = boxes[i];
+        const box = boxes[i];
+        const boxSet = new Set(box);
+        if(boxSet.size < 9) {
+            errorBoxes[i] = box;
         }
     }
-    console.log(errorBoxes);
-    if(errorBoxes.length > 0) {
+    
+    if(Object.keys(errorBoxes).length > 0) {
         return errorBoxes;
     } else {
         return true;
@@ -101,14 +94,17 @@ function submit() {
     var miniBoxes = checkMiniBox();
     if(rows != true) {
         flag = false;
+        console.log('Rows');
         console.log(rows);
     }
     if(columns != true) {
         flag = false;
+        console.log('Columns');
         console.log(columns);
     }
     if(miniBoxes != true) {
         flag = false;
+        console.log('MiniBoxes');
         console.log(miniBoxes);
     }
     if(flag) {
